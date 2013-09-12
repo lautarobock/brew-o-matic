1e61f48e8f0dfd3660514326a9707d15607e34f7
Lautaro <lautaromail@gmail.com>
2013-09-12 17:19:05 +0200

    fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+//var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
