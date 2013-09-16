459d68d2e9febbbe729da37ae299047e42d0af93
lautaro <lautaromail@gmail.com>
2013-09-16 08:59:13 +0200

    detail. SRM. Round

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,13 @@
 (function() {
     
-    var SRM = ['#FFFFFF','#F3F993','#F5F75C','#F6F513','#EAE615','#E0D01B','#D5BC26','#CDAA37','#C1963C','#BE8C3A','#BE823A','#C17A37','#BF7138',
-        '#BC6733','#B26033','#A85839','#985336','#8D4C32','#7C452D','#6B3A1E','#5D341A','#4E2A0C','#4A2727','#361F1B','#261716','#231716','#19100F',
-        '#16100F','#120D0C','#100B0A','#050B0A'];
-    
+//    var SRM = ['#FFFFFF','#F3F993','#F5F75C','#F6F513','#EAE615','#E0D01B','#D5BC26','#CDAA37','#C1963C','#BE8C3A','#BE823A','#C17A37','#BF7138',
+//        '#BC6733','#B26033','#A85839','#985336','#8D4C32','#7C452D','#6B3A1E','#5D341A','#4E2A0C','#4A2727','#361F1B','#261716','#231716','#19100F',
+//        '#16100F','#120D0C','#100B0A','#050B0A'];
+
+    var SRM = ['#FFFFFF','FFE699' , '#FFD878' , '#FFCA5A' , '#FFBF42' , '#FBB123' , '#F8A600' , '#F39C00' , '#EA8F00' , '#E58500' , '#DE7C00',
+                '#D77200' , '#CF6900' , '#CB6200' , '#C35900' , '#BB5100' , '#B54C00' , '#B04500' , '#A63E00' , '#A13700' , '#9B3200',
+                '#952D00' , '#8E2900' , '#882300' , '#821E00' , '#7B1A00' , '#771900' , '#701400' , '#6A0E00' , '#660D00' , '#5E0B00',
+                '#5A0A02' , '#600903' , '#520907' , '#4C0505' , '#470606' , '#440607' , '#3F0708' , '#3B0607' , '#3A070B' , '#36080A'];
     var index = angular.module('index', ['ngResource']);
     
     index.controller("RecipeListCtrl", function ($scope) {
@@ -91,6 +95,10 @@
                 ]
             }
         };
+
+        function round(value, zeros) {
+            return Math.round(value*zeros)/zeros;
+        };
         
         $scope.removeFermentable = function(fermentable) {
             var index = $scope.recipe["FERMENTABLES"]["FERMENTABLE"].indexOf(fermentable);
@@ -126,7 +134,7 @@
             
             //Percetajes
             angular.forEach($scope.recipe.FERMENTABLES.FERMENTABLE,function(f) {
-                f.PERCENTAGE = f.AMOUNT/$scope.recipe.totalAmount*100;
+                f.PERCENTAGE = round(f.AMOUNT/$scope.recipe.totalAmount*100,100);
             });
             
             //Color
