91ce9a589f53e9da8163759c25da06ec4da9ae4f
Lautaro <lautaromail@gmail.com>
2013-10-30 13:09:17 +0100

    merge master

--- public/js/env.js
+++ public/js/env.js
@@ -2,7 +2,7 @@
     var env = angular.module("env", []);
     
     env.constant("version",'0.15');
-    env.constant("env",'dev');
-    env.constant("color",'success');
+    env.constant("env",'stage');
+    env.constant("color",'warning');
 
 })();
