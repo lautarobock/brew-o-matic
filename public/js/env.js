480c17de5c72317fda624b6494e6024805688213
lautaro <lautaromail@gmail.com>
2013-12-04 09:20:59 +0100

    change env

--- public/js/env.js
+++ public/js/env.js
@@ -1,7 +1,7 @@
 (function() {
     var env = angular.module("env", []);
     
-    env.constant("version",'0.15');
+    env.constant("version",'1.0.1-RC1');
     env.constant("env",'dev');
     env.constant("color",'warning');
 
