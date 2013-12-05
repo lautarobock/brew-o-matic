da13a67052b0442ec2064904f495f77a6f5c2b71
lautaro <lautaromail@gmail.com>
2013-12-05 09:04:25 +0100

    test autodeploy to appfog

--- public/js/env.js
+++ public/js/env.js
@@ -2,7 +2,7 @@
     var env = angular.module("env", []);
     
     env.constant("version",'1.0.1-RC1');
-    env.constant("env",'1.0.1-RC');
+    env.constant("env",'1.0.1-RC1');
     env.constant("color",'danger');
 
 
