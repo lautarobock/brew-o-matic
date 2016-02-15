87f65c2f6d75d65685bc4f8f4479771fd61fe89c
Lautaro Cozzani <lautaromail@gmail.com>
2016-02-15 09:05:30 -0300

    add water from brewersfriends

--- specs/brewersfriend.spec.js
+++ specs/brewersfriend.spec.js
@@ -24,4 +24,66 @@ describe("brewersfriend", function() {
 			expect(res.actualPitchRate).toBe(expected.actualPitchRate);
 		});
 	});
+
+	describe('bfWater', function() {
+		it('Should calculate diluted water', function() {
+			var output = {
+				diluted: new Array(6),
+				diff: new Array(6),
+				salts: new Array(6),
+				result: new Array(6),
+				adjusted: new Array(6)
+			};
+
+			var input = {
+				dilution:50,
+			    mashvolume: 79,
+			    source: [15,6,42,35,35,75,0],
+			    target: [60,5,55,10,95,0,0],
+			    CaCO3: 0,
+			    NaHCO3: 0,
+			    CaSO4: 0,
+			    CaCl2: 0,
+			    MgSO4: 0,
+			    NaCl: 0
+			};
+
+			bfWater.recalculate(input,output);
+
+			for ( var i=0; i<7; i++ ) {
+				expect(output.diluted[i]).toBe(bfWater.rounddecimal(input.source[i]/2,0));
+			}
+
+		});
+
+		it('Should calculate water profile', function() {
+			expect(bfWater.recalculate).toBeDefined();
+
+			var output = {
+				diluted: new Array(6),
+				diff: new Array(6),
+				salts: new Array(6),
+				result: new Array(6),
+				adjusted: new Array(6)
+			};
+
+			var input = {
+				dilution:10,
+			    mashvolume: 79,
+			    source: [15,6,42,35,35,75,0],
+			    target: [60,5,55,10,95,0,0],
+			    CaCO3: 0,
+			    NaHCO3: 0,
+			    CaSO4: 0,
+			    CaCl2: 0,
+			    MgSO4: 0,
+			    NaCl: 0
+			};
+
+			bfWater.recalculate(input,output);
+
+			console.log('input', JSON.stringify(input,null,4));
+			console.log('output',JSON.stringify(output,null,4));
+		});
+	});
 });
