39abd8ccb05d748f20029efa522730af2a772dd1
Lautaro Cozzani <lautaromail@gmail.com>
2017-04-06 14:54:07 -0300

    cross origin alowed

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ var mongoose = require('mongoose');
 var app = express();
 
 // all environments
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
