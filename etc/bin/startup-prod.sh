786450c6187855fe81ae821031b5b72db9d747f5
Lautaro Cozzani <lautaromail@gmail.com>
2018-02-09 12:43:57 -0300

    fix mongodb connection (error at login)

--- etc/bin/startup-prod.sh
+++ etc/bin/startup-prod.sh
@@ -1,2 +1,2 @@
-export MONGOLAB_URI=app:lac713@ds047948.mongolab.com:47948/af_brew-o-matic-lautaromail
+export MONGOLAB_URI=mongodb://app:lac713@ds047948.mongolab.com:47948/af_brew-o-matic-lautaromail
 node app.js
