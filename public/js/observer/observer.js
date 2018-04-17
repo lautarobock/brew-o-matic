6754677c261c831a6efa2b5f3c51add3c13ed72c
Lautaro Cozzani <lautaromail@gmail.com>
2018-04-17 08:22:16 -0300

    disable socket.io

--- public/js/observer/observer.js
+++ public/js/observer/observer.js
@@ -3,25 +3,20 @@
 	var observer = angular.module("observer", []);
 
     observer.run(function($location,pushListener) {
-        pushListener.socket = io.connect('http://'+$location.host());
+        // pushListener.socket = io.connect('http://'+$location.host());
     });
 
     observer.factory("pushListener", function() {
         return {
             on: function(id, callback) {
-                // this.socket.on(id, function (data) {
-                //     console.log("INFO", data);
-                //     callback(data);
-                // });
-                this.socket.on(id, callback);
+                // this.socket.on(id, callback);
             },
             off: function(id, fn) {
-                if ( fn ) {
-                    this.socket.removeListener(id,fn);
-                } else {
-                    this.socket.removeAllListeners(id);
-                }
-
+                // if ( fn ) {
+                //     this.socket.removeListener(id,fn);
+                // } else {
+                //     this.socket.removeAllListeners(id);
+                // }
             }
         };
     });
--- routes/push.js
+++ routes/push.js
@@ -6,22 +6,21 @@ var util = require("../public/js/util/util.js");
 var _socket = [];
 
 exports.initOn = function(app) {
-	var i = io.listen(app);
-    i.set('transports', ['websocket','xhr-polling']);
-	i.sockets.on("connection", function(socket) {
-		_socket.push(socket);
-		console.log("INFO", "Connect WebSocket", _socket.length);
+	// var i = io.listen(app);
+    // i.set('transports', ['websocket','xhr-polling']);
+	// i.sockets.on("connection", function(socket) {
+	// 	_socket.push(socket);
+	// 	console.log("INFO", "Connect WebSocket", _socket.length);
 		
-		socket.on('disconnect', function () {
-		    util.Arrays.remove(_socket,socket);
-		    console.log("INFO", "Disconnect WebSocket", _socket.length);
-		});
-	});
+	// 	socket.on('disconnect', function () {
+	// 	    util.Arrays.remove(_socket,socket);
+	// 	    console.log("INFO", "Disconnect WebSocket", _socket.length);
+	// 	});
+	// });
 };
 
 exports.emit = function(id, data) {
-	for ( var i=0; i<_socket.length; i++ ) {
-		_socket[i].emit(id, data);	
-	}
-	
+	// for ( var i=0; i<_socket.length; i++ ) {
+	// 	_socket[i].emit(id, data);	
+	// }
 };
