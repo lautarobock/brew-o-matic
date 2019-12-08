0a5c57f32fe744dca6188bf806110754d9024547
Juan Pablo Giménez <jpg@blameless.com>
2019-12-08 20:50:10 -0300

    Revert "change 'nazi mode' to 'strict mode'"
    This reverts commit c8987138823187226144335be51f8f2b0010fb6b.

--- domain/model.js
+++ domain/model.js
@@ -252,7 +252,7 @@ const RecipeSchema = new Schema({
     PRIMARY_TEMP: Number, //@deprecated parece sin uso
     BIAB: String, //@deprecated parece sin uso
     IBUCalcMethod: String, //@deprecated parece sin uso
-    StyleStrictMode: String, //@deprecated parece sin uso, cree uno diferente
+    StyleNaziMode: String, //@deprecated parece sin uso, cree uno diferente
     IsNoChill: String, //@deprecated parece sin uso
     GrainUnits: String, //@deprecated parece sin uso
     HopsUnits: String, //@deprecated parece sin uso
@@ -281,7 +281,7 @@ const RecipeSchema = new Schema({
     //Start own properties
     isPublic: Boolean,
     publishDate: Date,
-    strictMode: Boolean,
+    naziMode: Boolean,
     phPreBoil: Number,
     phPostBoil: Number,
     cloneFrom: {type:String, ref:'Recipe'},
