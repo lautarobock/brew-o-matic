f9380397e5a812a755502588f494a5fe4216d66a
Lautaro Cozzani <lautaro.cozzani@quorumsoftware.com>
2025-03-17 11:30:45 -0300

    Fix typos in Spanish labels and improve error handling in tilt update function

--- routes/tilt.js
+++ routes/tilt.js
@@ -6,31 +6,38 @@ var model = require('../domain/model.js');
  */
 exports.updateTilt = function(req, res) {
     model.Recipe.findOne({_id:req.params.id}).exec(function(err,recipe) {
-        if (recipe.tiltValues.length > 2) {
-            //si los ultimos 3 valoes son iguales elimino el anteultimo
-            var lastPos = recipe.tiltValues.length-1;
-            var last = recipe.tiltValues[lastPos];
-            var prev = recipe.tiltValues[lastPos-1];
-            if ( last.temp === prev.temp && prev.temp === fahrenheitToCelsius(req.body.Temp) && 
-                last.sg === prev.sg && prev.sg === parseFloat(req.body.SG)) {
-                recipe.tiltValues.pop();
+        console.log('UPDATE TILT', req.params.id);
+        console.log(req.body);
+        try {
+            if (recipe.tiltValues.length > 2) {
+                //si los ultimos 3 valoes son iguales elimino el anteultimo
+                var lastPos = recipe.tiltValues.length-1;
+                var last = recipe.tiltValues[lastPos];
+                var prev = recipe.tiltValues[lastPos-1];
+                if ( last.temp === prev.temp && prev.temp === fahrenheitToCelsius(req.body.Temp) && 
+                    last.sg === prev.sg && prev.sg === parseFloat(req.body.SG)) {
+                    recipe.tiltValues.pop();
+                }
             }
+            recipe.tiltValues.push({
+                date: new Date(),
+                sg: parseFloat(req.body.SG),
+                temp: fahrenheitToCelsius(req.body.Temp)
+            })
+            recipe.save(function(err) {
+                if ( err ) {
+                    res.send(500,{error: 'Error al actializr TITL en la receta'});
+                } else {
+                    console.log('Params', req.params.id);
+                    console.log('Query', JSON.stringify(req.query));
+                    console.log('Body', JSON.stringify(req.body));
+                    res.send({"result":"success", "row": recipe.tiltValues[recipe.tiltValues.length-1]});
+                }
+            });
+        } catch (error) {
+            console.error('Error al actializr TITL en la receta', error);
         }
-        recipe.tiltValues.push({
-            date: new Date(),
-            sg: parseFloat(req.body.SG),
-            temp: fahrenheitToCelsius(req.body.Temp)
-        })
-        recipe.save(function(err) {
-            if ( err ) {
-                res.send(500,{error: 'Error al actializr TITL en la receta'});
-            } else {
-                console.log('Params', req.params.id);
-                console.log('Query', JSON.stringify(req.query));
-                console.log('Body', JSON.stringify(req.body));
-                res.send({"result":"success", "row": recipe.tiltValues[recipe.tiltValues.length-1]});
-            }
-        });
+        
     });
 };
 
